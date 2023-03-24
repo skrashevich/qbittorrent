66f53fa86e2a78a3d569a0817e072b63f1fb6a03
Sergey Krashevich <svk@svk.su>
2023-03-24 12:16:56 +0300

    build ci

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ if [[ -z ${1} ]]; then
 fi
 
 image=$(basename "$(git rev-parse --show-toplevel)")
-image="registry.svk.app/skrashevich/qbittorrent"
-docker buildx build -f "./linux-${1}.Dockerfile" -t "${image}" $(for i in $(jq -r 'to_entries[] | [(.key | ascii_upcase),.value] | join("=")' < VERSION.json); do out+="--build-arg $i " ; done; echo $out;out="") --load .
+image="ghcr.io/skrashevich/qbittorrent:${1}"
+docker buildx build -f "./linux-${1}.Dockerfile" -t "${image}" $(for i in $(jq -r 'to_entries[] | [(.key | ascii_upcase),.value] | join("=")' < VERSION.json); do out+="--build-arg $i " ; done; echo $out;out="") "--${2}" .
