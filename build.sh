670de263ae583db713d867ef47b3dbf42e372f4f
Sergey Krashevich <svk@svk.su>
2023-03-25 16:38:03 +0300

    add gha cache to build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ fi
 
 image=$(basename "$(git rev-parse --show-toplevel)")
 image="ghcr.io/skrashevich/qbittorrent:${1}"
-docker buildx build -f "./linux-${1}.Dockerfile" -t "${image}" $(for i in $(jq -r 'to_entries[] | [(.key | ascii_upcase),.value] | join("=")' < VERSION.json); do out+="--build-arg $i " ; done; echo $out;out="") "--${2}" .
+docker buildx build -f "./linux-${1}.Dockerfile" -t "${image}" --cache-to 'type=gha,mode=max' --cache-from 'type=gha' $(for i in $(jq -r 'to_entries[] | [(.key | ascii_upcase),.value] | join("=")' < VERSION.json); do out+="--build-arg $i " ; done; echo $out;out="") "--${2}" .
